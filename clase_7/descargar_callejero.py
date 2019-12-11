76263dbf3f7f8740b266a79f6668ee7e914acb48
felipe <felipegonz@gmail.com>
2019-12-11 14:32:22 -0300

    actualizar clase 7

--- clase_7/descargar_callejero.py
+++ clase_7/descargar_callejero.py
@@ -1,3 +1,4 @@
 import osmnx as ox
 G = ox.graph_from_place('Buenos Aires, Argentina', network_type='drive')
 ox.save_graph_shapefile(G, filename='caba_red',folder='../carto')
+ox.save_graphml(G, filename='caba_callejero.graphml',folder='../carto')
